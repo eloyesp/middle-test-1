766b964bc1a28122342cea00dcd5bc09d6d0aabd
Eloy Espinaco <eloyesp@gmail.com>
2012-11-21 16:36:54 -0300

    A passing test to ease refactory.

--- test/test_xyz_service.rb
+++ test/test_xyz_service.rb
@@ -3,14 +3,28 @@ require 'minitest/mock'
 
 class TestXYZService < MiniTest::Unit::TestCase
 
-  def test_xyz_filename
+  def setup
+    @object = MiniTest::Mock.new
+    @object.expect :publish_on, Date.new(2012, 11, 8)
+    @object.expect :xyz_category_prefix, "SEL"
+    @object.expect :kind, "a_blog"
+    @object.expect :id, 1538
+    @object.expect :title, "a realy good title"
+  end
 
-    object = MiniTest::Mock.new
-    # [day of month zero-padded][three-letter prefix] \
-    # _[kind]_[age_if_kind_personal]_[target.id] \
-    # _[8 random chars]_[10 first chars of title].jpg
-    filename = XYZService.xyz_filename object
-    assert_match /[0-9]/, filename
+  def test_xyz_filename_with_no_person
+    @object.expect :personal?, false
+    filename = XYZService.xyz_filename @object
+    assert_match /08SELablog_1538_[a-z0-9]{8}_arealygood.jpg/, filename
+    @object.verify
+  end
+
+  def test_xyz_filename_with_a_person
+    @object.expect :personal?, true
+    @object.expect :age, 19
+    filename = XYZService.xyz_filename @object
+    assert_match /08SELablog_019_1538_[a-z0-9]{8}_arealygood.jpg/, filename
+    @object.verify
   end
 
 end
