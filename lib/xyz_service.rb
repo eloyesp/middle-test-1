5de7b6f5b2574b80a538a86b45d330dfba7b21ed
Eloy Espinaco <eloyesp@gmail.com>
2012-11-21 18:01:24 -0300

    Fix documentation position.
    Made it Yard compatible.

--- lib/xyz_service.rb
+++ lib/xyz_service.rb
@@ -1,10 +1,14 @@
 module XYZService
 
+  # Build a filename for the target with the following format:
+  #   [day of month zero-padded][three-letter prefix] \
+  #   _[kind]_[age_if_kind_personal]_[target.id] \
+  #   _[8 random chars]_[10 first chars of title].jpg
+  #
+  # @params  target the filename
+  # @return [String] filename of the target
+
   def self.xyz_filename(target)
-    # File format:
-    # [day of month zero-padded][three-letter prefix] \
-    # _[kind]_[age_if_kind_personal]_[target.id] \
-    # _[8 random chars]_[10 first chars of title].jpg
     filename = "#{target.publish_on.strftime("%d")}"
     filename << "#{target.xyz_category_prefix}"
     filename << "#{target.kind.gsub("_", "")}"
