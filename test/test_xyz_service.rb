4eea0e85d024be5ae5d233fac177ed93b1a4d30a
Eloy Espinaco <eloyesp@gmail.com>
2012-11-21 19:06:15 -0300

    Another test for the big names.

--- test/test_xyz_service.rb
+++ test/test_xyz_service.rb
@@ -9,10 +9,10 @@ class TestXYZService < MiniTest::Unit::TestCase
     @object.expect :xyz_category_prefix, "SEL"
     @object.expect :kind, "a_blog"
     @object.expect :id, 1538
-    @object.expect :title, "a realy good title"
   end
 
   def test_xyz_filename_with_no_person
+    @object.expect :title, "a realy good title"
     @object.expect :personal?, false
     filename = XYZService.xyz_filename @object
     assert_match /08SELablog_1538_[a-z0-9]{8}_arealygood.jpg/, filename
@@ -20,10 +20,19 @@ class TestXYZService < MiniTest::Unit::TestCase
   end
 
   def test_xyz_filename_with_a_person
+    @object.expect :title, "a realy good title"
     @object.expect :personal?, true
     @object.expect :age, 19
     filename = XYZService.xyz_filename @object
     assert_match /08SELablog_019_1538_[a-z0-9]{8}_arealygood.jpg/, filename
+    @object.verify
+  end
+
+  def test_with_tiny_title
+    @object.expect :title, "a title"
+    @object.expect :personal?, false
+    filename = XYZService.xyz_filename @object
+    assert_match /08SELablog_1538_[a-z0-9]{8}_atitle.jpg/, filename
     @object.verify
   end
 
